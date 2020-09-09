fc97c7821a879277a2a80f3cf6cccbac087037ee
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-09 20:24:36 +0200

    Update styling

--- src/fakeData.ts
+++ src/fakeData.ts
@@ -38,7 +38,7 @@ export const eventsData: Event[] = [
     date: ['2017', '11', '03'],
     link: { text: 'www.gaite-lyrique.net', src: 'https://gaite-lyrique.net' },
     imgUrl: food,
-    color: '#eaea85',
+    color: '#ebeb89',
     geoCoords: {
       lat: 48.83,
       lng: 2.3652,
@@ -84,7 +84,7 @@ export const eventsData: Event[] = [
     date: ['2018', '02', '24'],
     link: { text: 'www.gaite-lyrique.net', src: 'https://gaite-lyrique.net' },
     imgUrl: fashion,
-    color: '#e69eb7',
+    color: '#e7a2ba',
     geoCoords: {
       lat: 48.86,
       lng: 2.28,
@@ -107,7 +107,7 @@ export const eventsData: Event[] = [
     date: ['2017', '10', '03'],
     link: { text: 'www.gaite-lyrique.net', src: 'https://gaite-lyrique.net' },
     imgUrl: night,
-    color: '#c3a2af',
+    color: '#c7a8b4',
     geoCoords: {
       lat: 48.88,
       lng: 2.32,
